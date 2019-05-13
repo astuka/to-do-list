a04d6f30e3b0e4285fda8610b78b7263878ff3f6
astuka <jrobins855@gmail.com>
2019-05-13 14:19:41 -0700

    fixed having two different variables for localstorage

--- todo.js
+++ todo.js
@@ -1,7 +1,35 @@
 //storage
 let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : []
 localStorage.setItem('items',JSON.stringify(itemsArray))
-const data = JSON.parse(localStorage.getItem('items'))
+
+//click close button to hide current list item
+var close = document.getElementsByClassName("close");
+
+//find list items in localStorage
+function openStorage() {
+    for (i=0; i < itemsArray.length; i++){
+        var li = document.createElement("li");
+        var inputValue = itemsArray[i];
+        var t = document.createTextNode(inputValue);
+        li.appendChild(t);
+        document.getElementById("List").appendChild(li);
+        var span = document.createElement("span");
+        var txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        //span.onclick = delTask();
+        span.appendChild(txt);
+        li.appendChild(span);
+    }
+
+    for (i=0;i < close.length; i++) {
+         close[i].onclick = function() {
+             var div = this.parentElement;
+             div.style.display = "none";
+             itemsArray.splice(i-1,1);
+             localStorage.setItem('items',JSON.stringify(itemsArray));
+         }
+     }
+}
 
 // create close button, assign it to each list element
 var myNodelist = document.getElementsByTagName("li");
@@ -14,8 +42,11 @@ for (i = 0; i < myNodelist.length; i++){
     myNodelist[i].appendChild(span);
 }
 
-//click close button to hide current list item
-var close = document.getElementsByClassName("close");
+// function delTask(div){
+//     //var div = this.parentElement;
+//     div.style.display = "none";
+// }
+
 
 //create new list item when clicking on the "Add" button
 function newElement() {
@@ -35,6 +66,7 @@ function newElement() {
     var span = document.createElement("span");
     var txt = document.createTextNode("\u00D7");
     span.className = "close";
+    //span.onclick = delTask()
     span.appendChild(txt);
     li.appendChild(span);
 
@@ -48,30 +80,6 @@ function newElement() {
     }
 }
 
-//find list items in localStorage
-function openStorage() {
-    for (i=0; i < data.length; i++){
-        var li = document.createElement("li");
-        var inputValue = data[i];
-        var t = document.createTextNode(inputValue);
-        li.appendChild(t);
-        document.getElementById("List").appendChild(li);
-        var span = document.createElement("span");
-        var txt = document.createTextNode("\u00D7");
-        span.className = "close";
-        span.appendChild(txt);
-        li.appendChild(span);
-    }
-
-    for (i=0;i < close.length; i++) {
-        close[i].onclick = function() {
-            var div = this.parentElement;
-            div.style.display = "none";
-            itemsArray.splice(i-1,1);
-            localStorage.setItem('items',JSON.stringify(itemsArray));
-        }
-    }
-}
 
 // document.querySelector("#myInput").addEventListener("keyup", event => {
 //     if(event.key == "Enter"){
@@ -79,5 +87,3 @@ function openStorage() {
 //     }
 //         event.preventDefault();
 // })
-
-// to remove an item from localStorage: localStorage.removeItem('myCat');
