b7005a1ba796f299c59a9acd9e00326d1dfa4daf
astuka <jrobins855@gmail.com>
2019-05-15 12:01:56 -0700

    numbers now work for proper deletion
    (for some reason)

--- todo.js
+++ todo.js
@@ -11,13 +11,14 @@ function openStorage() {
     for (i=0; i < itemsArray.length; i++){
         var li = document.createElement("li");
         var inputValue = itemsArray[i];
+        // var strValue = String(inputValue);
         var t = document.createTextNode(inputValue);
         li.appendChild(t);
         document.getElementById("List").appendChild(li);
         var span = document.createElement("span");
         var txt = document.createTextNode("\u00D7");
         span.className = "close";
-        span.setAttribute('onclick','delTask(itemsArray[i])');
+        span.setAttribute('onclick','delTask(' + inputValue+ ')');
         span.appendChild(txt);
         li.appendChild(span);
     }
@@ -27,6 +28,7 @@ function openStorage() {
 function newTask() {
     var li = document.createElement("li");
     var inputValue = document.getElementById("myInput").value;
+    // var strValue = String(inputValue);
     if (inputValue === ''){
         alert("You must write something!");
     } else {
@@ -36,41 +38,25 @@ function newTask() {
         li.appendChild(t);
         document.getElementById("List").appendChild(li);
     }
-    document.getElementById("myInput").value = "";
-
     var span = document.createElement("span");
     var txt = document.createTextNode("\u00D7");
     span.className = "close";
-    span.setAttribute('onclick','delTask(inputValue)');
+    span.setAttribute('onclick','delTask(' + inputValue + ')');
     span.appendChild(txt);
     li.appendChild(span);
+    document.getElementById("myInput").value = "";
 }
 
-//close.setAttribute('onclick','delTask(inputValue)')
-
+//delete a task
 function delTask(e){
     for (i=0; i < myList.length;i++){
-        if (myList[i].textContent == e){
+        if (myList[i].textContent == e + "×"){
             itemsArray.splice(i,1);
             localStorage.setItem('items',JSON.stringify(itemsArray));
             myList[i].style.display = "none";
         }
     }
 }
-
-
-// for (i=0;i < close.length; i++) {
-//     close[i].onclick = function() {
-//         var div = this.parentElement;
-//         div.style.display = "none";
-//         for (j=0; j < close.length;j++){
-//             if (div.value == itemsArray[i]){
-//                 itemsArray.splice(i-1,1);
-//                 localStorage.setItem('items',JSON.stringify(itemsArray));
-//             }
-//         }
-//     }
-// }
 
 
 
